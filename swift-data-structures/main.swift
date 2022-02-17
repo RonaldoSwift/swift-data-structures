ea89424784a327a0dc6c6106181ca752a776964c
Ronaldo Andre <ronaldovh99@gmail.com>
2022-02-17 18:43:40 -0500

    Segunda parte agregando el Heap

--- swift-data-structures/main.swift
+++ swift-data-structures/main.swift
@@ -13,6 +13,10 @@ import Foundation
  Entrada y salida de texto en Swift: https://www.programiz.com/swift-programming/basic-input-output
  */
 
+struct DequeFaseDeProduccion{
+     
+}
+
 
 var isMainMenuRunning: Bool = true
 var option: String
@@ -22,9 +26,10 @@ while(isMainMenuRunning){
     print("\n ----------------------------------------------------")
     print("|               EMPRESA DE CONFESIONES               |")
     print("|----------------------------------------------------|")
-    print("|               1)Fase de Produccion                 |")
-    print("|               2)                                   |")
-    print("|               3)Salir                              |")
+    print("|              1)Fase de Produccion                  |")
+    print("|              2)Imprecion de Bicola                 |")
+    print("|              3)Imprecion de Cola de Prioriedad     |")
+    print("|              4)Salir                               |")
     print("|____________________________________________________|\n")
     
     print(" Elija una opcion 1-3")
@@ -32,10 +37,22 @@ while(isMainMenuRunning){
     
     switch(option){
     case "1":
+        
         break
     case "2":
+        
         break
     case "3":
+        
+        print("|-------------------------------------------------------------------------------------------------------------------------------|")
+        print("|                                                      SECUENCIA EN MINUTOS                                                     |")
+        print("|-------------------------------------------------------------------------------------------------------------------------------|")
+        print("| NUMERO | COLOR | PRIORIEDAD | TEJIDO | COSTURA | ACABADO | LAVADO | ZURCIDO | VAPORIZADO | ACABADO | EMBOLSADO | TIEMPO TOTAL |")
+        print("| CHOMPA |       |            |        |         |         |        |         |            |   FINAL |           | FABRICACION  |")
+
+        break
+    case "4":
+        print("Gracias!!!")
         break
     default:
         print("Error")
